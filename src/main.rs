34ffa7f6a42727623cd129d6bd2ad135d4018aac
criskell <cris.moraes.ds@gmail.com>
2025-06-30 01:51:54 -0300

    feat: implement BootInfoFrameAllocator

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,28 @@
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
 use osdev_rust::println;
-use x86_64::structures::paging::Translate;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use osdev_rust::memory;
-    use x86_64::VirtAddr;
+    use osdev_rust::memory::BootInfoFrameAllocator;
+    use x86_64::{VirtAddr, structures::paging::Page};
 
     println!("Hello World!");
 
     osdev_rust::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeef000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe {
+        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
     }
 
     #[cfg(test)]
